e59595ce47881f561c421b6c4a36c7ba3a526aca
y8nju <107169943+y8nju@users.noreply.github.com>
2022-08-24 12:53:32 +0900

    First: setting

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Alert from 'react-bootstrap/Alert';
+
+function App() {
+  return (
+    <div className="App">
+      <Alert key="" variant="primary">
+      Hey
+        </Alert>
+    </div>
+  );
+}
+
+export default App;
