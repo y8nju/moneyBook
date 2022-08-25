b237eaa18b3c3e9dd141d399d51ed9699f679223
y8nju <107169943+y8nju@users.noreply.github.com>
2022-08-25 11:41:58 +0900

    Add: password bcrypt 암호화

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors';
-import api from './router/api.js'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import account from './router/account.js'
 
 const app = express();
+dotenv.config()
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {dbName: 'moneybook'}).catch((err) => {
+	console.log('failed' + err.message);
+});
 
 app.use(cors());
 app.use(morgan('[Server] :date[iso] :method :url :status (:response-time ms)' ))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use('/api', api)
+app.use('/api/account', account)
 
 app.listen(8080, ()=> {
 	console.log('server start')
@@ -25,4 +32,5 @@ app.listen(8080, ()=> {
 	mongoose
 	dotenv: npm i dotenv
 	jsonwebtoken: npm i jsonwebtoken
+	bcrypt: npm i bcryp(암호화 모듈)
 */
